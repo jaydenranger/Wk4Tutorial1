937c7061d771d2b1443adfcc597c09b1fc350483
Jayden Ranger <10012125@BCS.NET.NZ>
2017-07-31 11:14:57 +1200

    Wk4Tutorial1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace wk41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            
+            Console.WriteLine("Whats your first name name?");
+            var firstName = Console.ReadLine();
+            Console.WriteLine(firstName);
+            Console.WriteLine("Whats your Last name name?");
+            var lastName = Console.ReadLine();
+            Console.WriteLine(lastName);
+            
+            //Start the program with Clear();
+           
+            
+            
+            //End the program with blank line and instructions
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+            
+        }
+    }
+}
